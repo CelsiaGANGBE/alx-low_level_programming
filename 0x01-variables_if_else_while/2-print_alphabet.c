89514cdda41d60fa9d5b99e905efa77af20d5475
CelsiaGANGBE <celsiagangbe@gmail.com>
2022-09-09 06:02:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * 
+ *Description: print alphabetic letter 
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
