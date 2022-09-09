f773138a88f7694033c2f143801e5242ffd9af2c
CelsiaGANGBE <celsiagangbe@gmail.com>
2022-09-09 06:14:07 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>                                                                                                                      
+
+                                                                                                                                        
+
+/*                                                                                                                                      
+ *
+ *                                                                                                                                       * main - Entry point                                                                                                                   
+ *
+ *                                                                                                                                        * Description: print alphabetic letter                                                                                                 
+ *
+ *                                                                                                                                         * Return: 0 (success)                                                                                                                  
+ *
+ *                                                                                                                                          */                                                                                                                                     
+
+int main(void)                                                                                                                          
+
+{                                                                                                                                       
+
+	        char ch;                                                                                                                        
+
+		                                                                                                                                        
+
+		        for (ch = 'a'; ch <= 'z'; ch++)                                                                                                 
+
+				                putchar(ch);                                                                                                            
+
+			        putchar('\n');                                                                                                                  
+
+				        return (0);                                                                                                                     
+
+} 
