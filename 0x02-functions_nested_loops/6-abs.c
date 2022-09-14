de5ffec5bf29459ebc878c2656a180c0a3bd88f4
CelsiaGANGBE <celsiagangbe@gmail.com>
2022-09-14 16:28:15 +0200

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#inclure « main.h »
+
+/**
+ * _abs - imprimer la valeur absolue d’un entier
+ * @num : le numéro à traiter
+ *
+ * Retour : la valeur absolue d’un entier
+ */
+int _abs(int num)
+{
+	si (nombre >= 0)
+		retour (nombre);
+	autre
+		retour (-num);
+}
