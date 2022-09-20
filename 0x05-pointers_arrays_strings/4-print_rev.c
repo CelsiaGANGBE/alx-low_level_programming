4f66af8746aafe0207a6dc88fa25dd44bf2a56c3
CelsiaGANGBE <celsiagangbe@gmail.com>
2022-09-20 23:41:20 +0200

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,9 @@ void print_rev(char *s)
 {
 	int i, j;
 
-	for (i = 0; s[i] != '\n'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		;
-
-	for (j = i-1; j >= 0; j--)
+	for (j = i - 1; j >= 0; j--)
 		_putchar(s[j]);
 	_putchar('\n');
 }
