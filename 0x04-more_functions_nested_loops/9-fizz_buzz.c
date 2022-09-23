89c6843146dcf1e4d5dd3128dec024ed6f20af8b
CelsiaGANGBE <celsiagangbe@gmail.com>
2022-09-23 21:53:52 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,20 +10,26 @@
 
 int main(void)
 {
-	 int i;
+	       int i;
 
         for (i = 0; i <= 100; i++)
         {
                 if ((i % 3) && (i % 5) == 0)
-                        printf("FizzBuzz");
+                {
+                   printf("FizzBuzz ");
+                }
                 else if ((i % 3) == 0)
-                        printf("Fizz");
+                {
+                 printf("Fizz ");
+                }
                 else if ((i % 5) == 0)
-                        printf("Buzz);
+                {
+                    printf("Buzz ");
+                }
                 else
-                        printf("%d", i);
-                printf(" ");
+                        printf("%d ", i);
+
         }
         printf("\n");
-    return (0);
+    return 0;
 }
