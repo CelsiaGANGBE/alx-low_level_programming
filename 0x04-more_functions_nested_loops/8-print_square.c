e560ddf297f42f76b8a460da5474860e8702c9cf
CelsiaGANGBE <celsiagangbe@gmail.com>
2022-09-23 20:57:20 +0200

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,6 +12,7 @@ void print_square(int size)
         if (size <= 0)
                 _putchar('\n');
         else
+	{
                 for (i = 1; i <= size; i++)
                         {
                                 for (j = 1; j <= size; j++)
@@ -20,4 +21,5 @@ void print_square(int size)
                                         }
                                 _putchar('\n');
                         }
+	}
 }
