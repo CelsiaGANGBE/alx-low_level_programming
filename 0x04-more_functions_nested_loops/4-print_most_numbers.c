84f381eeeddeae35a2b39a6ddb474cdc8e16812d
CelsiaGANGBE <celsiagangbe@gmail.com>
2022-09-23 13:26:22 +0200

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,13 @@
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i == 4)
 			continue;
-		_putchar('0' + i);
+		else
+			_putchar('0' + i);
 	}
 	_putchar('\n');
 }
